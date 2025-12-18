7299097e37867a30a36c21a7aaec92f4c4d59321
unknown <2511500062@mahasiswa.atmaluhur.ac.id>
2025-12-18 11:29:50 +0700

    menambahkan tombol edit

---
+++ pertemuan-12/read.php
@@ -0,0 +1,32 @@
+<?php
+  session_start();
+  require 'koneksi.php';
+  require 'fungsi.php';
+
+$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+$q = mysqli_query($conn, $sql);
+$no = 1;
+
+?>
+
+<table border="1" cellpadding="8" cellspacing="0">
+
+    <tr>
+        <th>No</th>
+        <th>ID</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Pesan</th>
+        <th>Created At</th>
+    </tr>
+
+ <?php while ($row = mysqli_fetch_assoc($q)): ?>
+     <tr>
+         <td><?= $i++ ?></td>
+         <td><a href="edit.php?cid=<?= (int)$row['cid']; ?>">Edit</a></td>
+         <td><?= $row['cid']; ?></td>
+         <td><?= htmlspecialchars($row['cnama']);?></td>
+     </tr>
+
+    <?php endwhile; ?>
+</table>
